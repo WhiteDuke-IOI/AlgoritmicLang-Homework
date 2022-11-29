3ebc4bf1b024a32d3c5c921d86f722a74f4f59d1
Lerchek2502 <113030426+Lerchek2502@users.noreply.github.com>
2022-11-30 00:53:03 +0300

    ShakerSort

---
+++ Algoritm Home Work (ShakerSort)/Algoritm Home Work (ShakerSort)/shakersort.cpp
@@ -0,0 +1,27 @@
+﻿#include "shakersort.h"
+
+vector <int> shakersort(vector <int> vec) {
+	auto clock_start = chrono::high_resolution_clock::now();
+
+		int left = 0;
+		int right = vec.size() - 1;
+		while (left <= right) {
+			for (int i = right; i > left; --i) {
+				if (vec[i - 1] > vec[i]) {
+					swap(vec[i - 1], vec[i]);
+				}
+			}
+			++left;
+			for (int i = left; i < right; ++i) {
+				if (vec[i] > vec[i + 1]) {
+					swap(vec[i], vec[i + 1]);
+				}
+			}
+			--right;
+		}
+
+	auto clock_stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::nanoseconds>(clock_stop - clock_start);
+	cout << duration.count();
+	return vec;
+}
---
+++ Algoritm Home Work (ShakerSort)/Algoritm Home Work (ShakerSort)/shakersort.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+vector <int> shakersort(vector <int> vec);
