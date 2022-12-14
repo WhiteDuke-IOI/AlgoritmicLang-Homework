1fcc2edadef9480c5f12699088fa4fd4743d01bd
IlyaShipov <i.shipov2016@yandex.ru>
2022-12-14 12:57:54 +0300

    Presentation

--- AlgoritmicLang-HomeWork/AlgoritmicLang-HomeWork/Source.cpp
+++ AlgoritmicLang-HomeWork/AlgoritmicLang-HomeWork/Source.cpp
@@ -10,9 +10,9 @@
 using namespace std;
 
 int main() {
-	const int count = 10000;
+	const int count = 100;
 	const int lower = 0;
-	const int upper = 100000;
+	const int upper = 1000;
 
 	vector <int> vec0;
 	for (int i = 0; i < count; i++) {
@@ -21,42 +21,42 @@ int main() {
 
 	vector <int> vec1, vec2, vec3, vec4, vec5, vec6;
 
-	/*for (int q : vec0) { cout << q << " "; }
-	cout << endl << endl;*/
+	for (int q : vec0) { cout << q << " "; }
+	cout << endl << endl;
 
 	cout << "gnomesort: ";
 	vec1 = gnomesort(vec0);
-	/*cout << " - ";
-	for (int q : vec1) { cout << q << " "; }*/
+	cout << " - ";
+	for (int q : vec1) { cout << q << " "; }
 	cout << endl << endl;
 
 	cout << "combsort: ";
 	vec2 = combsort(vec0);
-	/*cout << " - ";
-	for (int q : vec2) { cout << q << " "; }*/
+	cout << " - ";
+	for (int q : vec2) { cout << q << " "; }
 	cout << endl << endl;
 
 	cout << "heapsort: ";
 	vec3 = heapsort(vec0);
-	/*cout << " - ";
-	for (int q : vec3) { cout << q << " "; }*/
+	cout << " - ";
+	for (int q : vec3) { cout << q << " "; }
 	cout << endl << endl;
 
 	cout << "shakersort: ";
 	vec4 = shakersort(vec0);
-	/*cout << " - ";
-	for (int q : vec4) { cout << q << " "; }*/
+	cout << " - ";
+	for (int q : vec4) { cout << q << " "; }
 	cout << endl << endl;
 
 	cout << "bucketsort: ";
 	vec5 = bucketsort(vec0);
-	/*cout << " - ";
-	for (int q : vec5) { cout << q << " "; }*/
+	cout << " - ";
+	for (int q : vec5) { cout << q << " "; }
 	cout << endl << endl;
 
 	cout << "smoothsort: ";
 	vec6 = smoothSort(vec0);
-	/*cout << " - ";
-	for (int q : vec6) { cout << q << " "; }*/
+	cout << " - ";
+	for (int q : vec6) { cout << q << " "; }
 	cout << endl << endl;
 }
